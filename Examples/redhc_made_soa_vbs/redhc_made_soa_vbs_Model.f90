3ef985a7d2ddec4c19139ed96d67dcd4adb797d7
AndrewJConley <aconley@ucar.edu>
2021-01-21 13:29:51 -0700

    add Examples directory along with instructions in the README

---
+++ Examples/redhc_made_soa_vbs/redhc_made_soa_vbs_Model.f90
@@ -0,0 +1,20 @@
+MODULE redhc_made_soa_vbs_Model
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!  Completely defines the model redhc_made_soa_vbs
+!    by using all the associated modules
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  USE redhc_made_soa_vbs_Precision
+  USE redhc_made_soa_vbs_Parameters
+  USE redhc_made_soa_vbs_Global
+  USE redhc_made_soa_vbs_Function
+  USE redhc_made_soa_vbs_Integrator
+  USE redhc_made_soa_vbs_Rates
+  USE redhc_made_soa_vbs_Jacobian
+  USE redhc_made_soa_vbs_LinearAlgebra
+  USE redhc_made_soa_vbs_Monitor
+  USE redhc_made_soa_vbs_Util
+
+END MODULE redhc_made_soa_vbs_Model
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # KPP_to_CAMP
+
+Parses KPP files under a significant number of assumptions, due to lack of a clear syntax.
+
+Usage
+
+```
+python3 convert.py file_in.kpp [file_out.kpp]
+```
+
+
